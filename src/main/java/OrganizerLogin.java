f409f64dff0d86a1789f1bfac59bc0d05465cb4e
senirudilmith4 <dilmith.seniru0@gmail.com>
2025-12-02 01:46:11 +0530

    Added user logins

---
+++ src/main/java/OrganizerLogin.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class OrganizerLogin {
+
+    private static Map<String,String> organizers = new HashMap<>();
+    static {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("organizers.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    organizers.put(parts[0].trim(), parts[1].trim());
+                }
+            }
+        }catch (IOException e) {
+            System.out.println("Error loading organizer credentials: " + e.getMessage());
+        }
+
+    }
+    public static boolean authenticate() {
+        if (organizers.isEmpty()) {
+            System.out.println("No organizers found. Contact admin.");
+            return false;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===== Organizer Login =====");
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (organizers.containsKey(username) && organizers.get(username).equals(password)) {
+            System.out.println("Login successful!\n");
+            return true;
+        } else {
+            System.out.println("Incorrect username or password.\n");
+            return false;
+        }
+    }
+
+}
--- src/main/java/TeamMateApp.java
+++ src/main/java/TeamMateApp.java
@@ -7,39 +7,91 @@ import java.util.*;
 public class TeamMateApp {
 
     private static final Scanner scanner = new Scanner(System.in);
-    private static List<Participant> participants = new ArrayList<>();
     private static List<Team> formedTeams = new ArrayList<>();
     private static AppController appController = new AppController();
+    private static OrganizerLogin organizerLogin = new OrganizerLogin();
+
+
 
     public static void main(String[] args) {
         printBanner();
 
         boolean running = true;
         while (running) {
+            printLogin();
+            int choice = getIntInput("Enter choice: ",1,3);
+            switch (choice) {
+                case 1:
+                    try {
+                        if (organizerLogin.authenticate()==true) {
+                            organizerInterface();
+                        }else {
+                            System.out.println("You are not authenticated");
+                        }
+                    }
+                    catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
 
-            printMenu();
-            int choice = getIntInput("Enter choice: ", 1, 7);
+                    break;
+                case 2:
+                    participantInterface();
+                    break;
+                case 3:
+                    running = false;
+                    break;
+            }
+        }
+        System.out.println("\nThank you for using TeamMate! Goodbye.");
+        scanner.close();
+    }
+
+    public static void participantInterface() {
+
+        boolean running = true;
+        while (running) {
+
+            participantMenu();
+            int choice = getIntInput("Enter choice: ", 1, 3);
 
             switch (choice) {
                 case 1:
                     appController.completeSurvey();
                     break;
                 case 2:
-                   // generateSampleData();
+                    //  view Survey Results();
                     break;
                 case 3:
-                   appController.loadAllParticipantsAtStart();
+                    running = false;
                     break;
-                case 4:
+            }
+        }
+
+        System.out.println("\nThank you for using TeamMate! Goodbye.");
+        scanner.close();
+    }
+
+    public static void organizerInterface() {
+
+        boolean running = true;
+        while (running) {
+
+            organizerMenu();
+            int choice = getIntInput("Enter choice: ", 1, 6);
+
+            switch (choice) {
+                case 1:
+                    appController.viewParticipants();
+                    break;
+                case 2:
+                    appController.loadAllParticipantsAtStart();
+                    break;
+                case 3:
                     appController.formTeams();
                     break;
-                case 5:
-                 //   viewTeams();
-                    break;
+                case 4:
+                   // appController.viewFormedTeams();
                 case 6:
-                  //  saveResults();
-                    break;
-                case 7:
                     running = false;
                     break;
             }
@@ -65,19 +117,36 @@ public class TeamMateApp {
 
     }
 
-    private static void printMenu() {
+    private static void participantMenu() {
         System.out.println("\n┌─────────────── MAIN MENU ───────────────┐");
         System.out.println("│  1. Add Participant                     │");
-        System.out.println("│  2. View added participants             │");
-        System.out.println("│  3. Load participants from CSV          │");
-        System.out.println("│  4. Form teams                          │");
-        System.out.println("│  5. View formed teams                   │");
-        System.out.println("│  6. Save results to file                │");
+        System.out.println("│  2. View Survey Results                 │");
         System.out.println("│  7. Exit                                │");
+        System.out.println("└─────────────────────────────────────────┘");
+    }
+
+    private static void organizerMenu() {
+        System.out.println("\n┌─────────────── MAIN MENU ───────────────┐");
+        System.out.println("│  1. View added participants             │");
+        System.out.println("│  2. Load participants from CSV          │");
+        System.out.println("│  3. Form teams                          │");
+        System.out.println("│  4. View formed teams                   │");
+        System.out.println("│  5. Save results to file                │");
+        System.out.println("│  6. Exit                                │");
         System.out.println("└─────────────────────────────────────────┘");
         System.out.printf("  [Loaded: %d participants, %d teams]\n",
                 AppController.participants.size(), formedTeams.size());
     }
+
+
+    private static void printLogin(){
+        System.out.println("\n┌───────────── Login ───────────────┐");
+        System.out.println("│  1. Organizer                     │");
+        System.out.println("│  2. Participant                   │");
+        System.out.println("│  3. Exit                          │");
+        System.out.println("└───────────────────────────────────┘");
+    }
+
 
     private static int getIntInput(String prompt, int min, int max) {
         while (true) {
